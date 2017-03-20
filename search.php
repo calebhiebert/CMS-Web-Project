5cf55fb89591e794eb65bec31f2b5ab711f1e27b
Caleb Hiebert <calebkhiebert@gmail.com>
2017-03-20 11:51:20 -0500

    search

--- search.php
+++ search.php
@@ -15,29 +15,64 @@ $entities = getEntities(9, 0);
 
 <?php startblock('body') ?>
     <div class="container mt-4" id="entity-list">
+        <label>
+            Search
+            <input type="text" class="form-control" v-model="query">
+        </label>
         <ul class="list-group">
-            <li class="list-group-item">
-
-            </li>
+            <li class="list-group-item" v-for="entity in entities.entities"><a href="/entity/{{ entity.id }}">{{ entity.name }}</a></li>
         </ul>
-        <nav>
-            <ul class="pagination">
-                <li class="page-item" v-for=""></li>
-            </ul>
-        </nav>
+        <button type="button" class="btn btn-outline-secondary" v-if="page > 0" @click="prevPage">Previous Page</button>
+        Page: {{ page }}
+        <button type="button" class="btn btn-outline-secondary" @click="nextPage">Next Page</button>
     </div>
 <?php endblock() ?>
 
 <?php startblock('script') ?>
 <script src="https://vuejs.org/js/vue.js"></script>
 <script>
-    var page = 0;
-
     var app = new Vue({
         el: '#entity-list',
         data: {
-            entities: []
+            page: 0,
+            query: '',
+            entities: {}
+        },
+        watch: {
+            query: function (newQuery) {
+                getEntities(newQuery, this.page, 5);
+            }
+        },
+        methods: {
+            nextPage: function () {
+                this.page ++;
+                getEntities(this.query, this.page, 5);
+            },
+            prevPage: function () {
+                this.page --;
+                if(this.page < 0)
+                    this.page = 0;
+
+                getEntities(this.query, this.page, 5);
+            }
         }
     });
+
+    getEntities('', 0, 5);
+    
+    function getEntities(query, page, num) {
+        var data = "query=" + encodeURIComponent(query) + '&page=' + encodeURIComponent(page) + '&num=' + encodeURIComponent(num);
+        var xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4 && this.status == 200) {
+                app.entities = JSON.parse(this.responseText);
+            }
+        });
+
+        xhr.open("POST", "/api/entity-search");
+        xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+        xhr.send(data);
+    }
 </script>
 <?php endblock() ?>
