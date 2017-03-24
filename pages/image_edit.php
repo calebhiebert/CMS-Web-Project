0b1ed4189c3735ae7046965553064ab0d4516de1
Caleb Hiebert <calebkhiebert@gmail.com>
2017-03-24 11:19:06 -0500

    actually delete image from drive

--- pages/image_edit.php
+++ pages/image_edit.php
@@ -9,11 +9,20 @@ if(!$token_valid) {
 $imageId = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $entity = filter_input(INPUT_GET, 'entityid', FILTER_VALIDATE_INT);
 
-print_r($_GET);
-
 if(isset($_GET['delete'])) {
+    $image = getSingle('SELECT Id, FileExt FROM Pictures WHERE Id = :id', ['id'=>$imageId]);
+
     deleteImage($imageId);
+
+    //delete the files
+    foreach (IMAGE_FILE_SIZES as $NAME => $SIZE) {
+        unlink(IMAGE_LOCATION.DIRECTORY_SEPARATOR.$NAME.DIRECTORY_SEPARATOR.$image['Id'].'.'.$image['FileExt']);
+    }
+
     header('Location: '.$_SERVER['HTTP_REFERER']);
     exit;
 }
+
+print_r($_GET);
+
 ?>
